171f10d60dd96594aa045f5d78f7065d992af8e2
Anton Petkov <anton.petkoff@gmail.com>
2017-10-14 18:37:24 +0300

    Fix tests

--- exercises/02/count-divisors.scm
+++ exercises/02/count-divisors.scm
@@ -12,7 +12,7 @@
    (check = (count-divisors 19) 2) ; 1 19
    (check = (count-divisors 42) 8) ; 1 2 3 6 7 14 21 42
    (check = (count-divisors 661) 2) ; 1 661
-   (check = (count-divisors 666) 8) ; 1 2 4 8 83 166 332 664
+   (check = (count-divisors 666) 12) ; 1 2 3 6 9 18 37 74 111 222 333 666
    (check = (count-divisors 1337) 4) ; 1 7 191 1337
    (check = (count-divisors 65515) 4) ; 1 5 13103 65515
    (check = (count-divisors 1234567) 4))) ; 1 127 9721 1234567
