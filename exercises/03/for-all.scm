86c39a7affde1c39a6da26c1746426a4a23c4396
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2017-10-23 16:55:31 +0300

    Fix parentheses in for-all? tests

---
+++ exercises/03/for-all.scm
@@ -0,0 +1,15 @@
+(require rackunit rackunit/text-ui)
+
+(define for-all-tests
+  (test-suite
+   "Tests for for-all? predicate"
+
+   (check-true (for-all? (lambda (x) (> x 0) 2 98)))
+   (check-true (for-all? (lambda (x) (< x 0) -10 -1)))
+   (check-true (for-all? (lambda (x) (= 0 (* x 0))) -3 15))
+
+   (check-false (for-all? (lambda (x) (= x 3)) 1 5))
+   (check-false (for-all? (lambda (x) (= x 13)) 1 5))
+   (check-false (for-all? (lambda (x) (< x 3)) -5 42))))
+
+(run-tests for-all-tests)
