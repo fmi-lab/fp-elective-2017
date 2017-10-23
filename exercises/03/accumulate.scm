57926bd9f1268c72f40ce8ba578c26c86f59e3ab
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2017-10-23 10:43:47 +0300

    Add tests for accumulate

--- exercises/03/accumulate.scm
+++ exercises/03/accumulate.scm
@@ -1,23 +1,23 @@
 (require rackunit rackunit/text-ui)
 
-(define compose-tests
+(define accumulate-tests
   (test-suite
-   "Tests for compose"
+   "Tests for accumulate"
 
    (define (identity x) x)
    (define (inc x) (+ x 1))
    (define (square x) (* x x))
 
-   (check = ((compose identity identity) 0) 0)
-   (check = ((compose identity inc) 0) 1)
-   (check = ((compose inc identity) 0) 1)
+   (check = (accumulate + 0 identity 1 inc 5) 15)
+   (check = (accumulate + 0 square 1 inc 5) 55)
 
-   (check = ((compose inc inc) 0) 2)
-   (check = ((compose square square) 2) 16)
+   (check = (accumulate * 0 identity 1 inc 5) 0)
+   (check = (accumulate * 1 identity 0 inc 5) 0)
+   (check = (accumulate * 1 identity 1 inc 5) 120)
 
-   (check = ((compose square inc) 6) 49)
-   (check = ((compose inc square) 6) 37)
+   (check = (accumulate (lambda (x acc)
+                          (if (even? x) (inc acc) acc))
+                        0 identity 0 inc 10) 6)
+   (check = (accumulate + 0 (lambda (x) (if (even? x) 1 0)) 0 inc 10) 6)))
 
-   (check = ((compose (compose square inc) inc) 6) 64)))
-
-(run-tests compose-tests)
+(run-tests accumulate-tests)
