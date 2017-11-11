da8433bcd51b5f0126c426660b07935644098a8e
Anton Petkov <anton.petkoff@gmail.com>
2017-11-11 09:30:26 +0200

    chunk

--- exercises/05/chunk.scm
+++ exercises/05/chunk.scm
@@ -1,5 +1,23 @@
 (require rackunit rackunit/text-ui)
 
+; Би било много удобно, ако разполагахме с готова функция,
+; която отчупва частица (chunk) с произволна големина n
+; от подадения списък l.
+; За наша радост има 2 такива функции - take и drop.
+; take дава нов списък с първите n елемента на списъка l,
+; а drop дава останалата част на списъка.
+(define (chunk l n)
+  (define (iter chunks remaining size)
+    (cond ((null? remaining)
+            chunks)
+          ((< size n)
+            (iter (cons remaining chunks) '() 0))
+          (else
+            (iter (cons (take remaining n) chunks)
+                        (drop remaining n)
+                        (- size n)))))
+  (reverse (iter '() l (length l))))
+
 (define chunk-tests
   (test-suite
     "Tests for chunk"
