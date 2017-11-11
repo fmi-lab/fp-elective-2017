e6df2b1829047fb3743daf055a449eb305502035
Anton Petkov <anton.petkoff@gmail.com>
2017-11-11 09:43:22 +0200

    Fix zip tests and implement zip

--- exercises/05/zip.scm
+++ exercises/05/zip.scm
@@ -1,12 +1,20 @@
 (require rackunit rackunit/text-ui)
 
+(define (zip a b)
+  (if (or (null? a) (null? b))
+      '()
+      (cons (list (car a) (car b))
+            (zip  (cdr a) (cdr b)))))
+
 (define zip-tests
   (test-suite
     "Tests for zip"
 
-    (check-equal? (zip '(() (6 6 6))) '())
-    (check-equal? (zip '((1 1 1) (2 2))) '((1 2) (1 2)))
-    (check-equal? (zip '((1 3 5) (2 4 6))) '((1 2) (3 4) (5 6)))
-    (check-equal? (zip '((1 3 5 7 9) (2 4 6))) '((1 2) (3 4) (5 6)))))
+    (check-equal? (zip '() '(6 6 6)) '())
+    (check-equal? (zip '(1 2) '(3 4)) '((1 3) (2 4)))
+    (check-equal? (zip '(1 1 1) '(2 2)) '((1 2) (1 2)))
+    (check-equal? (zip '(1 3 5) '(2 4 6)) '((1 2) (3 4) (5 6)))
+    (check-equal? (zip '(1 3 5 7 9) '(2 4 6)) '((1 2) (3 4) (5 6)))
+))
 
 (run-tests zip-tests)
