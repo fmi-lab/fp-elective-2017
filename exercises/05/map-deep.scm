54e1207e5668752cbd1762677e12f96dd537c73b
Anton Petkov <anton.petkoff@gmail.com>
2017-11-11 09:53:33 +0200

    map-deep

--- exercises/05/map-deep.scm
+++ exercises/05/map-deep.scm
@@ -3,14 +3,21 @@
 (define (square x) (* x x))
 (define (cube x) (* x x x))
 
+(define (map-deep f l)
+  (cond ((null? l)       '())
+        ((list? (car l)) (cons (map-deep f (car l))
+                               (map-deep f (cdr l))))
+        (else            (cons (f (car l))
+                               (map-deep f (cdr l))))))
+
 (define map-deep-tests
   (test-suite
     "Tests for map-deep"
 
-    (check-equal? (map-deep cube '() '()))
+    (check-equal? (map-deep cube '()) '())
     (check-equal? (map-deep square '((1 2 (3 4)) 5)) '((1 4 (9 16)) 25))
     (check-equal? (map-deep square '((((2)) 1 ((4) 3)) (9)))
                   '((((4)) 1 ((16) 9)) (81)))
-    (check-equal? (map-deep square '(3 2 ((3) 4))) '(27 8 ((27) 64)))))
+    (check-equal? (map-deep cube '(3 2 ((3) 4))) '(27 8 ((27) 64)))))
 
 (run-tests map-deep-tests)
