d5a373352b0e7e48e0e259c9eeb06db2b5fd6cff
Dimitar Uzunov <dimitar.uzunov.dev@gmail.com>
2017-12-28 21:19:26 +0200

    Refactor solution to third task

--- homework/task-3.scm
+++ homework/task-3.scm
@@ -3,15 +3,6 @@
 (define (average a b)
   (/ (+ a b) 2.0))
 
-(define (signum x)
-  (cond ((< x 0) -1)
-        ((> x 0) 1)
-        (else 0)))
-
-(define (same-sign? x y)
-  (= (signum x)
-     (signum y)))
-
 (define (approx-zero? x eps)
   (< (abs x) eps))
 
@@ -22,8 +13,8 @@
       (cond ((or (approx-zero? f-mid eps)
                  (approx-zero? (- b a) eps))
              (cons mid iteration))
-            ((same-sign? (f a) f-mid) (helper mid b (+ iteration 1)))
-            (else (helper a mid (+ iteration 1))))))
+            ((< (* (f a) f-mid) 0) (helper a mid (+ iteration 1)))
+            (else (helper mid b (+ iteration 1))))))
 
   (helper a b 1))
 
@@ -41,13 +32,15 @@
   (helper a b 1))
 
 (define (newton-method f a b eps)
-  (define (derive f dx)
+  (define (derive f)
+    (define dx 1e-6)
+
     (lambda (x)
       (/ (- (f (+ x dx))
             (f x))
          dx)))
 
-  (define f-derived (derive f eps))
+  (define f-derived (derive f))
 
   (define (next-approx previous)
     (- previous
@@ -65,14 +58,14 @@
   (newton-method f a b eps))
 
 (define (compare-methods f a b eps)
-  (list (binary-search f a b eps)
-        (secant-method f a b eps)
-        (newton-method f a b eps)))
+  (list (cdr (binary-search f a b eps))
+        (cdr (secant-method f a b eps))
+        (cdr (newton-method f a b eps))))
 
-(define eps 0.000001)
+(define eps 1e-6)
 
 (compare-methods (lambda (x) (- (exp x) (* 3 x))) 0 1 eps)
-(compare-methods (lambda (x) (expt (- x 2) 3)) 0 5 0.1)
+(compare-methods (lambda (x) (expt (- x 2) 3)) 0 5 1e-1)
 (compare-methods (lambda (x) (- (sqrt x) (* 2 (log x)))) 1 5 eps)
 (compare-methods (lambda (x) (- (exp (sqrt x)) (* 2 x))) 5 10 eps)
 
@@ -81,12 +74,12 @@
    "Tests for task 3"
 
    (check-= (car (find-root (lambda (x) (- (exp x) (* 3 x))) 0 1 eps))
-            0.6191 0.0001)
+            0.6191 1e-4)
    (check-= (car (find-root (lambda (x) (expt (- x 2) 3)) 0 5 eps))
             2 eps)
    (check-= (car (find-root (lambda (x) (- (sqrt x) (* 2 (log x)))) 1 5 eps))
-            2.044 0.001)
+            2.044 1e-3)
    (check-= (car (find-root (lambda (x) (- (exp (sqrt x)) (* 2 x))) 5 10 eps))
-            6.853 0.001)))
+            6.853 1e-3)))
 
 (run-tests task-3-tests)
